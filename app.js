faae241eef71e3a04fe3217d089b5deb66d89844
Thiago Magano <thiagomagano1993@outlook.com>
2020-06-08 19:57:46 -0300

    Adicao de templates, adiccao de rotas, testes de logica, imagens

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
-const _ = require('lodash');
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 
-//classes
-//classe pergunta
-class Pergunta {
-
-    constructor(i, alt) {
-        this.id = i;
-        this.alternativa = alt;
-    }
-
-    checaAlternativa(alt) {
-        if (alt === "a" || alt === "b" || alt == "c") {
-            return true;
-        } else if (alt === "d" || alt === "e" || alt == "f") {
-            return false;
-        } else {
-            return "ocorreu um erro";
-        }
-    }
-}
-
-//classe reposta
+//classe resposta
 class Resposta {
     constructor(id, titulo, desc, urlImg, li) {
         this.id = id;
@@ -31,6 +19,20 @@ class Resposta {
         this.lista = li
     }
 }
+
+function checaAlternativa(alt) {
+    if (alt === "a" || alt === "b" || alt === "c") {
+        return true;
+    } else if (alt === "d" || alt === "e" || alt === "f") {
+        return false;
+    } else {
+        return 'erro';
+    }
+};
+
+//classe reposta
+
+
 
 msg1 = "'Ela é tão ela'. Um dos principais motivos de eu me apaixonar por ti foi o fato de tu ser um ser humano muito singular e único, tu é do teu próprio jeito e ninguem chega perto de ti, amo o fato de tu ser tão especial! s2"
 
@@ -52,40 +54,91 @@ msg9 = "Amo quando nós fica agarradinho, quietinho no quartinho.. é, tu é a p
 
 msg10 = "E esse look ai? sempre bem vestida, cheia do ouro e das roupas mas estilosas que todas queriam ter, mas só a mimi tem. Te amo por esse teu estilo só teu, te completam e explica muito o que tu é, linda e estilosa demais!! s2"
 
-//seta respostas
-const res01 = new Resposta(1, "Mimi Única", msg1, url1, [true, true, true, true, true]);
-const res02 = new Resposta(2, "Mimi Criativa", msg2, url2, [false, true, true, true, true]);
-const res03 = new Resposta(3, "Mimi Determinada", msg3, url3[false, false, true, true, true]);
-const res04 = new Resposta(4, "Mimi Linda", msg4, url4, [false, false, false, true, true]);
-const res05 = new Resposta(5, "Mimi Solidária", msg5, url5[false, false, false, false, true]);
-const res06 = new Resposta(6, "Mimi Companheira", msg6, url6[false, false, false, false, false]);
-const res07 = new Resposta(7, "Mimi Gostosa", msg7, url7[true, true, true, true, false]);
-const res08 = new Resposta(8, "Mimi Comunicativa", msg8, url8, [true, true, true, false, false]);
-const res09 = new Resposta(9, "Mimi Carinhosa", "bla bla bla 1", msg9, url9, [true, true, false, false, false]);
-const res10 = new Resposta(10, "Mimi Estilosa", msg10, url10, [true, false, false, false, false]);
+
+const url = ["",
+        "../images/mimi1.jpg",
+        "../images/mimi2.jpg",
+        "../images/mimi3.jpg",
+        "../images/mimi4.jpg",
+        "../images/mimi5.jpg",
+        "../images/mimi6.jpg",
+        "../images/mimi7.jpg",
+        "../images/mimi8.jpg",
+        "../images/mimi9.jpg",
+        "../images/mimi10.jpg"
+    ]
+    //seta respostas
+const res01 = new Resposta(1, "Mimi Única", msg1, url[1], [true, true, true, true, true]);
+const res02 = new Resposta(2, "Mimi Criativa", msg2, url[2], [false, true, true, true, true]);
+const res03 = new Resposta(3, "Mimi Determinada", msg3, url[3], [false, false, true, true, true]);
+const res04 = new Resposta(4, "Mimi Linda", msg4, url[4], [false, false, false, true, true]);
+const res05 = new Resposta(5, "Mimi Solidária", msg5, url[5], [false, false, false, false, true]);
+const res06 = new Resposta(6, "Mimi Companheira", msg6, url[6], [false, false, false, false, false]);
+const res07 = new Resposta(7, "Mimi Gostosa", msg7, url[7], [true, true, true, true, false]);
+const res08 = new Resposta(8, "Mimi Comunicativa", msg8, url[8], [true, true, true, false, false]);
+const res09 = new Resposta(9, "Mimi Carinhosa", msg9, url[9], [true, true, false, false, false]);
+const res10 = new Resposta(10, "Mimi Estilosa", msg10, url[10], [true, false, false, false, false]);
+
+var possiveisRepostas = [res01, res02, res03, res04, res05, res06, res07, res08, res09, res10]
 
 
 function checaReposta(lista) {
-    if (_.isEqual(lista, resposta1.lista)) {
-        return resposta1;
-    } else if (_.isEqual(lista, resposta2.lista)) {
-        return resposta2
-    } else if (_.isEqual(lista, resposta3.lista)) {
-        return resposta3
-    } else if (_.isEqual(lista, resposta4.lista)) {
-        return resposta4;
+    if (_.isEqual(lista, res01.lista)) {
+        return res01;
+    } else if (_.isEqual(lista, res02.lista)) {
+        return res02
+    } else if (_.isEqual(lista, res03.lista)) {
+        return res03
+    } else if (_.isEqual(lista, res04.lista)) {
+        return res04;
+    } else if (_.isEqual(lista, res05.lista)) {
+        return res05;
+    } else if (_.isEqual(lista, res06.lista)) {
+        return res06;
+    } else if (_.isEqual(lista, res07.lista)) {
+        return res07;
+    } else if (_.isEqual(lista, res08.lista)) {
+        return res08;
+    } else if (_.isEqual(lista, res09.lista)) {
+        return res09;
+    } else if (_.isEqual(lista, res10.lista)) {
+        return res10;
     } else {
-        return "Não encontrado";
+        return _.sample(possiveisRepostas);
     }
 }
 
+app.get("/", function(req, res) {
 
-var listaFinal = [checaAlternativa("f"), checaAlternativa("c")];
-console.log(lista1);
 
-var resFinal = checaReposta(listaFinal);
+    res.render('index');
+});
 
-//devolve pro usuário
-resFinal.titulo
-resFinal.descricao
-resFinal.urlImg
+app.post("/", function(req, res) {
+    var listaFinal = [];
+
+    listaFinal.push(checaAlternativa("e"))
+    listaFinal.push(checaAlternativa("f"))
+    listaFinal.push(checaAlternativa("a"))
+    listaFinal.push(checaAlternativa("b"))
+    listaFinal.push(checaAlternativa("f"))
+
+    console.log(listaFinal);
+
+
+    var respostaFinal = checaReposta(listaFinal);
+
+    console.log(respostaFinal);
+
+    res.render("resposta", {
+        respTitulo: respostaFinal.titulo,
+        respDesc: respostaFinal.descricao,
+        respImg: respostaFinal.urlImg,
+    });
+});
+
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
